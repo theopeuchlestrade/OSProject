cfb963085a5970734af21b2c401ba208183e7fb0
Julian63800 <pet.dog63800@gmail.com>
2022-05-03 10:36:51 +0200

    No bugs and warnings, dumped at 113383 :(

--- exo1/exo1.c
+++ exo1/exo1.c
@@ -15,12 +15,13 @@ void *thread_compute(void *arg)
 
     int i = range->start;
     printf("Valeur de i : %d\n",i);
-    for (range->start; i < range->end; i++)
+    while(i <= range->end)
     {
         printf("thread %d is computing : %d\n", range->thread_id, i);
         range->tab[i] = get_time_of_flight_recursive(i, range->cache);
-
+        printf("Value of %d get calculated !\n", i);
         // sleep(1);
+        i++;
     }
 
     return NULL;
@@ -31,12 +32,9 @@ int main(int argc, char *argv[])
 
     if (argc == 2)
     {
-
         int input_size = atoi(argv[1]);
         printf("SIZE = %d\n", input_size);
-
-        //Get number of iterations
-        int nb_iteration = (int) input_size/NB_THREAD;
+        
         /* prepare cache  */
         cache_t cache;
         cache_init(&cache);
@@ -44,30 +42,35 @@ int main(int argc, char *argv[])
         pthread_t thread[NB_THREAD];
 
         int nb_thread = 0;
-        if(input_size < 10){
+        if(input_size <= 10){
             nb_thread = 1;
-        }else if(input_size < 20){
+        }else if(input_size <= 20){
             nb_thread = 2;
-        }else if(input_size < 30){
+        }else if(input_size <= 30){
             nb_thread = 3;
-        }else if(input_size < 40){
+        }else if(input_size <= 40){
             nb_thread = 4;
-        }else if(input_size < 50){
+        }else if(input_size <= 50){
             nb_thread = 5;
-        }else if(input_size < 60){
+        }else if(input_size <= 60){
             nb_thread = 6;
-        }else if(input_size < 70){
+        }else if(input_size <= 70){
             nb_thread = 7;
-        }else if(input_size < 80){
+        }else if(input_size <= 80){
             nb_thread = 8;
-        }else if(input_size < 90){
+        }else if(input_size <= 90){
             nb_thread = 9;
         }else{
             nb_thread = 10;
         }
 
+        //Get number of iterations
+        int nb_iteration = 0; 
+        if(input_size %2 == 1) nb_iteration = (int) (input_size + 1)/nb_thread;
+        else nb_iteration = (int) input_size/nb_thread; 
+
         // assert( syracuse != NULL );
-        int *syracuse = (int *)calloc((nb_iteration), sizeof(int)); // init a 0 ;
+        int *syracuse = (int *)calloc((input_size + 1), sizeof(int)); // init a 0 ;
         
         syracuse_args_t *schema = calloc(nb_thread, sizeof(syracuse_args_t));
         // Init threads args
@@ -76,8 +79,9 @@ int main(int argc, char *argv[])
             schema[num_thread].thread_id = num_thread,
             schema[num_thread].cache = &cache,
             schema[num_thread].tab = syracuse,
-            schema[num_thread].start = nb_iteration * num_thread,
-            schema[num_thread].end = nb_iteration * (num_thread+1) - 1;
+            schema[num_thread].start = nb_iteration * num_thread + 1;
+            if(num_thread == nb_thread -1){ schema[num_thread].end = input_size;}
+            else {schema[num_thread].end = nb_iteration * (num_thread+1);}
             printf("Num schema[%d] : start = %d, end = %d\n", num_thread, schema[num_thread].start, schema[num_thread].end);
         }
 
@@ -87,7 +91,9 @@ int main(int argc, char *argv[])
         for (int num_thread = 0; num_thread < nb_thread; num_thread++)
         {
             pthread_create(&thread[num_thread], NULL, &thread_compute, &schema[num_thread]);
+            printf("Creating the thread %d.\n", num_thread);
         }
+
 
         /* Wait threads */
         for (int num_thread = 0; num_thread < nb_thread; num_thread++)
@@ -97,10 +103,12 @@ int main(int argc, char *argv[])
 
         printf("End of threads \n");
 
-        // display_syracuse(syracuse);
+        //display_syracuse(syracuse);
 
+        for(int nb = 0; nb < input_size+1; nb++) printf("Syracuse de %d est %d.\n", nb, syracuse[nb]);
+        
         free(syracuse);
-        free(schema);
+        //free(schema);
         cache_destroy(&cache);
     }
     else
